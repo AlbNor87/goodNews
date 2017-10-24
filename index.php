ad0a9d83c92c5750f9c5c6b4b3c4c27a960c83b1
AlbNor87 <albert.norberg@gmail.com>
2017-10-24 18:07:13 +0200

    Added require in index.php to data.php and functions.php

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ declare(strict_types=1);
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-        <title></title>
+        <title>The Matrix News Feed</title>
     </head>
     <body>
 
