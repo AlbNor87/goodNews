000fcb19aadb3e35bcd41e67de0730ee2b97a85b
AlbNor87 <albert.norberg@gmail.com>
2017-10-28 21:36:04 +0200

    Added trinity to the database + minor css changes

--- data.php
+++ data.php
@@ -14,7 +14,7 @@ $posts = [
   ],
 
   [
-    'title' => "WE ARE THE CURE",
+    'title' => 'WE ARE THE CURE',
     'author' => 'Agent Smith',
     'avatar' => '<img src="../img/smith.jpg">',
     'content' => "I'd like to share a revelation that I've had during my time here. It came to me when I tried to classify your species and I realized that you're not actually mammals. Every mammal on this planet instinctively develops a natural equilibrium with the surrounding environment but you humans do not. You move to an area and you multiply and multiply until every natural resource is consumed and the only way you can survive is to spread to another area. There is another organism on this planet that follows the same pattern. Do you know what it is? A virus. Human beings are a disease, a cancer of this planet. You're a plague and we are the cure.",
@@ -27,6 +27,15 @@ $posts = [
     'author' => 'Neo',
     'avatar' => '<img src="../img/neo.png">',
     'content' => "I know you're out there. I can feel you now. I know that you're afraid... you're afraid of us. You're afraid of change. I don't know the future. I didn't come here to tell you how this is going to end. I came here to tell you how it's going to begin. I'm going to hang up this phone, and then I'm going to show these people what you don't want them to see. I'm going to show them a world without you. A world without rules and controls, without borders or boundaries. A world where anything is possible. Where we go from there is a choice I leave to you.",
+    'date' => '2017',
+    'likes' => 20,
+  ],
+
+  [
+    'title' => 'What is the matrix?',
+    'author' => 'Trinity',
+    'avatar' => '<img src="../img/trinity.png">',
+    'content' => "I know why you're here, Neo. I know what you've been doing. I know why you hardly sleep, why you live alone, and why night after night, you sit at your computer. You're looking for him. I know, because I was once looking for the same thing. And when he found me, he told me I wasn't really looking for him. I was looking for an answer. It's the question that drives us, Neo. It's the question that brought you here. You know the question, just as I did.",
     'date' => '2017',
     'likes' => 20,
   ],
